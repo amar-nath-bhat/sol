51b372d0b7b66fb3c69456f54a70bd57baade0d2
amarnathbhat <bhatamar8@gmail.com>
2024-07-15 10:19:43 +0530

    Add: File Reading

--- src/editor.rs
+++ src/editor.rs
@@ -4,7 +4,7 @@ use crossterm::event::{
     Event::{self, Key},
     KeyCode, KeyEvent, KeyEventKind, KeyModifiers,
 };
-use std::io::Error;
+use std::{env, io::Error};
 
 mod terminal;
 mod view;
@@ -28,6 +28,7 @@ pub struct Editor {
 impl Editor {
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
+        self.handle_args();
         let result = self.repl();
         Terminal::terminate().unwrap();
         result.unwrap();
@@ -121,5 +122,12 @@ impl Editor {
         Terminal::show_caret()?;
         Terminal::execute()?;
         Ok(())
+    }
+
+    fn handle_args(&mut self) {
+        let args: Vec<String> = env::args().collect();
+        if let Some(file_name) = args.get(1) {
+            self.view.load(file_name);
+        }
     }
 }
