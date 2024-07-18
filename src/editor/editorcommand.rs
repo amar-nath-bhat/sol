79f5d745d38e636a027b0ac5b0611e42e5afe16d
amarnathbhat <bhatamar8@gmail.com>
2024-07-18 21:58:27 +0530

    Update: Handle Invisible Characters

--- src/editor/editorcommand.rs
+++ src/editor/editorcommand.rs
@@ -38,10 +38,8 @@ impl TryFrom<Event> for EditorCommand {
                 _ => Err(format!("Key Code not supported: {code:?}")),
             },
             Event::Resize(width_u16, height_u16) => {
-                // clippy::as_conversions: Will run into problems for rare edge case systems where usize < u16
                 #[allow(clippy::as_conversions)]
                 let height = height_u16 as usize;
-                // clippy::as_conversions: Will run into problems for rare edge case systems where usize < u16
                 #[allow(clippy::as_conversions)]
                 let width = width_u16 as usize;
                 Ok(Self::Resize(Size { height, width }))
