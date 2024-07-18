29a3d5a232510c3afccbcc8244a0315a1de7a7d8
amarnathbhat <bhatamar8@gmail.com>
2024-07-18 21:45:43 +0530

    Update: Handling Graphemes

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -93,3 +93,12 @@ impl Terminal {
         Ok(())
     }
 }
+
+impl Position {
+    pub const fn saturating_sub(self, other: Self) -> Self {
+        Self {
+            col: self.col.saturating_sub(other.col),
+            row: self.row.saturating_sub(other.row),
+        }
+    }
+}
