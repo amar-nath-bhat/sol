0772b5eb309ad07cb645740b79efb9d8af4b33ee
amarnathbhat <bhatamar8@gmail.com>
2024-07-31 22:43:32 +0530

    Add: Highlight Numbers

--- src/editor/terminal/attribute.rs
+++ src/editor/terminal/attribute.rs
@@ -26,7 +26,7 @@ impl From<AnnotationType> for Attribute {
                     b: 0,
                 }),
             },
-            AnnotationType::Digit => Self {
+            AnnotationType::Number => Self {
                 foreground: Some(Color::Rgb {
                     r: 255,
                     g: 99,
